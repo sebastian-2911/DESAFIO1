af585f826e70300b1bc059fd4f4e2ea4778f72ad
Isaac <isaac20isaac241021@gmail.com>
2026-03-13 15:56:34 -0500

    creacion de movimientos de las piezas inclullendo la vercion alfa de las colisiones

---
+++ desafio1/funciones
@@ -0,0 +1,3 @@
+#include "funciones.h"
+
+funciones::funciones() {}
--- desafio1/main.cpp
+++ desafio1/main.cpp
@@ -2,45 +2,48 @@
 
 using namespace std;
 
-void dibujo_del_tablero(int filas, int columnas){
-    char** matriz;
-
-    matriz = new char*[filas];
+int** crear_tablero(int filas, int columnas)
+{
+    int** matriz = new int*[filas];
 
     for(int i = 0; i < filas; i++)
     {
-        matriz[i] = new char[columnas];
+        matriz[i] = new int[columnas];
     }
 
     for(int i = 0; i < filas; i++)
     {
         for(int j = 0; j < columnas; j++)
         {
-            matriz[i][j] = '.';
+            matriz[i][j] = 0;
         }
     }
 
+    return matriz;
+}
+void dibujar_tablero(int** tablero, int filas, int columnas)
+{
     for(int i = 0; i < filas; i++)
     {
-        for(int j = 0; j < columnas; j++)
+        for(int j = columnas - 1; j >= 0; j--)
         {
-            cout << matriz[i][j] << " ";
-        }
+            cout << ((tablero[i][0] >> j) & 1) << ".";// va moviendo ala deerecha los bits de un numero
+        }                                             //y los compara con & y 1 y si los 2 estan encendidos
+        //pondra un 1 si solo esta encendido el comaparador pondra un 0
         cout << endl;
     }
-
 }
-void comprobacion_de_medidas_del_tablero(int &filas, int &columnas){//como c++ no retorna 2 cosas modifico el contenido de las direciones de memoria
-    while (filas<8) {
-        cout<<"lo lamento pero el tamaño de filas del tablero es mu pequeno intente con un numero igual o mayoa 8 "<<endl;
-        cin>>filas;
+void comprobacion_de_medidas_del_tablero(int &filas, int &columnas)
+{
+    while (filas < 8) {
+        cout << "El numero de filas debe ser al menos 8: ";
+        cin >> filas;
     }
-    while(columnas % 8 != 0)
-    {
-        cout << "el numero de columnas No es multiplo de 8. Intente otra vez: ";
+
+    while (columnas > 32 || columnas % 8 != 0) {
+        cout << "Las columnas deben ser multiplo de 8 y menor o igual a 32: ";
         cin >> columnas;
     }
-
 }
 int* generar_pieza_O(int columnas_tablero) {
     int centro = columnas_tablero / 2;
@@ -181,8 +184,147 @@ int* generar_pieza_I(int columnas_tablero, int estado_rotacion) {
         return filas;
     }
 }
+int* generar_pieza_random(int columnas_tablero)
+{
+    int tipo = rand() % 7;
+    int rotacion =1;
+
+    if (tipo == 0)
+        return generar_pieza_O(columnas_tablero);
+
+    if (tipo == 1)
+        return generar_pieza_T(columnas_tablero, rotacion);
+
+    if (tipo == 2)
+        return generar_pieza_S(columnas_tablero, rotacion);
+
+    if (tipo == 3)
+        return generar_pieza_Z(columnas_tablero, rotacion);
+
+    if (tipo == 4)
+        return generar_pieza_L(columnas_tablero, rotacion);
+
+    if (tipo == 5)
+        return generar_pieza_J(columnas_tablero, rotacion);
+
+    return generar_pieza_I(columnas_tablero, rotacion);
+}
+int calcular_alto_pieza(int* pieza)
+{
+    int alto = 0;
+
+    // Consideramos máximo 4 filas
+    while(alto < 4 && pieza[alto] != 0)
+    {
+        alto++;
+    }
+
+    return alto;
+}
+void liberar_fila_del_tablero(int** tablero, int filas, int* pieza, int alto_pieza, int fila_actual)
+{
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        int fila = fila_actual - i;
+
+        // Nos aseguramos de no salirnos del tablero
+        if(fila >= 0 && fila < filas)
+        {
+            // Paso 1: OR entre la fila del tablero y la pieza
+            int fila_con_mascara = tablero[fila][0] | pieza[alto_pieza - 1 - i];
+
+            // Paso 2: AND con la máscara negada de la pieza
+            int resultado = fila_con_mascara & ~pieza[alto_pieza - 1 - i];
+
+            // Guardamos el resultado en el tablero
+            tablero[fila][0] = resultado;
+        }
+    }
+}
+void dibujar_pieza(int** tablero, int filas, int* pieza, int alto_pieza, int fila_actual)
+{
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        int fila = fila_actual - i;
+
+        if(fila >= 0 && fila < filas)
+        {
+            tablero[fila][0] |= pieza[alto_pieza - 1 - i];
+        }
+    }
+}
+bool frontera_izquierda(int* pieza, int alto_pieza, int columnas)
+{
+    int mascara = 1 << (columnas - 1);
+
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        if(pieza[i] & mascara)
+            return true;
+    }
+
+    return false;
+}
+bool frontera_derecha(int* pieza, int alto_pieza)
+{
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        if(pieza[i] & 1)
+            return true;
+    }
+
+    return false;
+}
+int** bajar_pieza(int** tablero, int filas, int* pieza, int alto_pieza, int &fila_actual)
+{
+    // verificar frontera inferior
+    if(fila_actual + 1 >= filas)
+        return tablero;
+
+    liberar_fila_del_tablero(tablero, filas, pieza, alto_pieza, fila_actual);
+
+    // bajar pieza
+    fila_actual++;
+
+    // dibujar pieza nueva
+    dibujar_pieza(tablero, filas, pieza, alto_pieza, fila_actual);
+
+    return tablero;
+}
+void mover_izquierda(int** tablero, int filas, int* pieza, int alto_pieza,
+                     int fila_actual, int &desplazamiento)
+{
+    // 1 borrar pieza actual
+    liberar_fila_del_tablero(tablero, filas, pieza, alto_pieza, fila_actual);
+
+    // 2 mover la máscara
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        pieza[i] <<= 1;
+    }
+
+    desplazamiento++;
+
+    // 3 dibujar la pieza otra vez
+    dibujar_pieza(tablero, filas, pieza, alto_pieza, fila_actual);
+}
+void mover_derecha(int** tablero, int filas, int* pieza, int alto_pieza,
+                   int fila_actual, int &desplazamiento)
+{
+    liberar_fila_del_tablero(tablero, filas, pieza, alto_pieza, fila_actual);
+
+    for(int i = 0; i < alto_pieza; i++)
+    {
+        pieza[i] >>= 1;
+    }
+
+    desplazamiento--;
+
+    dibujar_pieza(tablero, filas, pieza, alto_pieza, fila_actual);
+}
 int main()
 {
+
     int filas, columnas;
 
     cout << "Ingrese el tamano de filas en el tablero: " << endl;
@@ -190,9 +332,45 @@ int main()
 
     cout << "Ingrese el tamano de columnas del tablero: " << endl;
     cin >> columnas;
-    comprobacion_de_medidas_del_tablero(filas,columnas);
+    comprobacion_de_medidas_del_tablero(filas, columnas);
 
-    dibujo_del_tablero(filas, columnas);
+    int** tablero = crear_tablero(filas,columnas);
 
-    return 0;
+    int* pieza_actual = generar_pieza_random(columnas);
+    int alto_de_pieza = calcular_alto_pieza(pieza_actual);
+    int fila_actual = -1;
+    dibujar_tablero(tablero, filas, columnas);
+    char accion = ' ';
+    int desplazamiento=0;
+    while(accion != 'q')
+    {
+        cout << "-------------------------------------------" << endl;
+        dibujar_tablero(tablero, filas, columnas);
+
+        cout << "s: bajar | a: izquierda | d: derecha | q: salir\n";
+        cin >> accion;
+
+        if(accion == 's')
+        {
+            tablero = bajar_pieza(tablero, filas, pieza_actual, alto_de_pieza, fila_actual);
+        }
+
+        if(accion == 'a')
+        {
+            if(!frontera_izquierda(pieza_actual, alto_de_pieza, columnas))
+            {
+                mover_izquierda(tablero, filas, pieza_actual, alto_de_pieza, fila_actual, desplazamiento);
+            }
+        }
+
+        if(accion == 'd')
+        {
+            if(!frontera_derecha(pieza_actual, alto_de_pieza))
+            {
+                mover_derecha(tablero, filas, pieza_actual, alto_de_pieza, fila_actual, desplazamiento);
+            }
+        }
+    }
+
+
 }
